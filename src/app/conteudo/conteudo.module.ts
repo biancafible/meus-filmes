9138dd533baeaa7e85673e710cd6bf29f205b27d
Your Name <you@example.com>
2018-04-23 21:19:14 +0100

    ultimo

--- src/app/conteudo/conteudo.module.ts
+++ src/app/conteudo/conteudo.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http'
-
+import { FormsModule } from '@angular/forms'
+ 
 
 import { ListaFilmesComponent } from './lista-filmes/lista-filmes.component';
 import { FilmeCardComponent } from './filme-card/filme-card.component';
+import { FormPesquisaComponent } from './lista-filmes/form-pesquisa/form-pesquisa.component';
+
 
 import { FilmeService } from './filme.service';
 
@@ -12,9 +15,10 @@ import { FilmeService } from './filme.service';
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  declarations: [ListaFilmesComponent, FilmeCardComponent],
+  declarations: [ListaFilmesComponent, FilmeCardComponent, FormPesquisaComponent],
   exports: [
     ListaFilmesComponent,
   ],
--- src/app/conteudo/lista-filmes/lista-filmes.component.ts
+++ src/app/conteudo/lista-filmes/lista-filmes.component.ts
@@ -9,29 +9,20 @@ import { FilmeService } from '../filme.service';
 })
 export class ListaFilmesComponent implements OnInit {
 
-  listaFilmes: Filme[];
+  listaFilmes: Filme[] = [];
 
   constructor(
     private filmeService: FilmeService
   ) { }
 
   ngOnInit() {
-    this.filmeService.getFilmes().subscribe(data => {
-      data['Search'].map(filme => {
-        let film = {
-          'id': filme.imdbID,
-          'titulo': filme.Title,
-          'ano': filme.Year,
-          'tipo': filme.Type,
-          'poster': filme.poster
-        }
-        console.log(film)
-      });
-    });
+   
   }
   apagarFilme(filme: Filme): void {
     this.listaFilmes = this.listaFilmes.filter(item => item.id != filme.id);
 
   }
-
+  carregaLista(lista: Filme[]){
+    this.listaFilmes = lista;
+  }
 }
